a0332cf4d3f2ddc96d8195c41ebf8df6b0cfe042
Jose Ant. Aranda <jaarandaz@gmail.com>
2016-07-13 17:36:22 +0200

    New main page with credentials form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,57 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
-        <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato', sans-serif;
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8" />
+        <link rel="stylesheet" type="text/css" href="{{asset('css/app.css')}}">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/lib/bootstrap.css')}}">
+        {!!Html::script('js/lib/jquery.js')!!}
+        {!!Html::script('js/lib/bootstrap.js')!!}
+        {!!Html::script('js/lib/angular.js')!!}
+        {!!Html::script('js/app.js')!!}
+        <title>Launcher</title>
     </head>
     <body>
         <div class="container">
-            <div class="content">
-                <div class="title">AWS AMI Laucher</div>
+            <div class="page-header">
+                <h1>AWS AMI Laucher</h1>
+            </div>
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="panel panel-default">
+                        <div class="panel-heading">
+                            <h3 class="panel-title">Insert your AWS credentials</h3>
+                        </div>
+                        <div class="panel-body">
+                            {!! Form::open(['route' => ['launcher.launchAmi'], 'method' => 'post', 'class' => 'form-horizontal']) !!}
+                                <div class="form-group">
+                                    <label for="accesKey" class="col-sm-3 control-label">Access Key ID</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" name="accesKey" placeholder="AWS Access Key" required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <label for="secretKey" class="col-sm-3 control-label">Secret Key</label>
+                                    <div class="col-sm-9">
+                                        <input type="text" class="form-control" name="secretKey" placeholder="Secret Access Key" required>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <div class="col-sm-offset-3 col-sm-9">
+                                        <button type="submit" class="btn btn-primary">Launch!</button>
+                                    </div>
+                                </div>
+                            {!! Form::close() !!}
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
+        <footer class="footer">
+            <div class="container">
+                <p class="text-muted text-center"><small>Jose Ant. Aranda.</small></p>
+            </div>
+        </footer>
     </body>
 </html>
